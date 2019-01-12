aa5d018991430db8bbb18362d5e74a99bd3380fc
Xavier <xavrya10@gmail.com>
2019-01-12 15:28:45 -0500

    End of meeting Scrimmage
    Added arm program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/ScrimmageBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/ScrimmageBot.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode.bots;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.components.Arm;
 import org.firstinspires.ftc.teamcode.components.DriveTrain;
 import org.firstinspires.ftc.teamcode.components.Hoist;
 import org.firstinspires.ftc.teamcode.components.Logger;
@@ -42,6 +43,7 @@ public class ScrimmageBot extends Bot {
     public DriveTrain driveTrain = null;
     public Navigator navigator = null;
     public Hoist hoist = null;
+    public Arm arm = null;
 
     /* Constructor */
     public ScrimmageBot() {

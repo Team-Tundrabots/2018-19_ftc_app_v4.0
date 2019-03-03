54b7c33061c339ea1c9f9e53013a9c9a3a51fc7d
Carrick <lego2carr@gmail.com>
2019-03-03 10:54:34 -0500

    Creates a teleop with no file logging
    Removed file logging. Seems to prevent phone crashes and speeds up response time(kept logToTelemetry)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ops/game/Game_TeleOp_NOLOG.java
@@ -0,0 +1,147 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.ops.game;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.bots.GameBot;
+import org.firstinspires.ftc.teamcode.components.DriveTrain;
+
+
+@TeleOp(name="Game_TeleOp_NoLog", group="game")
+//@Disabled
+public class Game_TeleOp_NOLOG extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private GameBot robot = null;
+    private boolean logEnableTrace = false;
+    private boolean logToTelemetry = true;
+
+
+
+    @Override
+    public void runOpMode() {
+
+
+        robot = new GameBot(this, logEnableTrace, logToTelemetry);
+        robot.logger.logInfo("runOpMode", "===== [ Start Initializing ]");
+
+        robot.driveTrain.init(DriveTrain.InitType.INIT_4WD);
+        robot.driveTrain.disableEncoders();
+
+
+        robot.logger.logInfo("runOpMode", "===== [ Initialization Complete ]");
+        telemetry.update();
+
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        robot.logger.logInfo("runOpMode", "===== [ Start TeleOp ]");
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            // hoist controls
+            if (robot.hoist.isAvailable) {
+                if (gamepad1.dpad_down) {
+                    robot.hoist.extendContinuous(1);
+                } else if (gamepad1.dpad_up) {
+                    robot.hoist.contractContinuous(1);
+                } else {
+                    robot.hoist.stop();
+                }
+            }
+
+            // driveTrain controls
+            if (robot.driveTrain.isAvailable) {
+                double leftX = gamepad1.left_stick_x;
+                double leftY = gamepad1.left_stick_y;
+                double rightX = gamepad1.right_stick_x;
+                double rightY = gamepad1.right_stick_y;
+
+                robot.driveTrain.updateMotorsMechanumDrive(leftX, leftY, rightX, rightY);
+
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Left", "X (%.2f), Y (%.2f)", leftX, leftY);
+                telemetry.addData("Right", "X (%.2f), Y (%.2f)", rightX, rightY);
+            }
+
+
+
+            //PNP controls
+            if (robot.pnp.isAvailable){
+                if (gamepad1.right_stick_y > 0){
+                    robot.pnp.extend();
+                    /*if(soundCounterGold == 0){
+                        SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, goldSoundID);
+                        telemetry.addData("Playing", "Resource Gold");
+                        telemetry.update();
+                        soundCounterGold = soundCounterGold + 1;
+                    }
+                    */
+                }
+                else if(gamepad1.right_stick_y < 0) {
+                    robot.pnp.contract();
+                    /*if(soundCounterSilver == 0){
+                        SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, silverSoundID);
+                        telemetry.addData("Playing", "Resource Silver");
+                        telemetry.update();
+                        soundCounterSilver = soundCounterSilver + 1;
+                    }
+                    */
+
+                }
+                else{
+                    robot.pnp.pusher.setPower(0.0);
+                }
+                double arm_proportion = 0.7;
+                double new_left_trigger = gamepad1.left_trigger*arm_proportion;
+                double new_right_trigger = gamepad1.right_trigger*arm_proportion;
+                telemetry.addData("Left Trigger ", "Raw (%.2f), New (%.2f)", gamepad1.left_trigger, new_left_trigger);
+                telemetry.addData("Right Trigger", "Raw (%.2f), New (%.2f)", gamepad1.right_trigger, new_right_trigger);
+
+                robot.arm.crank.setPower(-new_left_trigger+new_right_trigger);
+            }
+            telemetry.update();
+
+        }
+
+        // Show the elapsed game time.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
+
+    }
+}
